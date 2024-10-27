210848775b3c01512e903ab36456329bef4ece91
Michael Kimberly Svrcek <107311268+mksvrcek@users.noreply.github.com>
2024-10-27 21:17:18 +0000

    1.0

--- index.js
+++ index.js
@@ -3,101 +3,80 @@
 var Service, Characteristic, HomebridgeAPI;
 const { HomebridgeDummyVersion } = require('./package.json');
 
-module.exports = function(homebridge) {
+module.exports = function (homebridge) {
 
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
   HomebridgeAPI = homebridge;
-  homebridge.registerAccessory("homebridge-dummy", "DummySwitch", DummySwitch);
+  homebridge.registerAccessory("homebridge-dummy-timer-custom-45n3u54u134n5iun", "DummyTimer", DummyTimer);
 }
 
 
-function DummySwitch(log, config) {
+function DummyTimer(log, config) {
   this.log = log;
   this.name = config.name;
-  this.stateful = config.stateful;
-  this.dimmer = config.dimmer;
+  this.dimmer = true;
+  this.isTimer = true;
+  this.defBrightness = config.brightness;
   this.brightness = config.brightness;
   this.brightnessStorageKey = this.name + "Brightness";
-  this.reverse = config.reverse;
-  this.time = config.time ? config.time : 1000;		
-  this.resettable = config.resettable;
   this.timer = null;
-  this.random = config.random;
   this.disableLogging = config.disableLogging;
 
   if (this.dimmer) {
-	this._service = new Service.Lightbulb(this.name);
-	this.modelString = "Dummy Dimmer";
-  } else {
-	this._service = new Service.Switch(this.name);
-	this.modelString = "Dummy Switch";
+    this._service = new Service.Lightbulb(this.name);
+    this.modelString = "Dummy Timer";
   }
-  
+
   this.informationService = new Service.AccessoryInformation();
   this.informationService
-      .setCharacteristic(Characteristic.Manufacturer, 'Homebridge')
-      .setCharacteristic(Characteristic.Model, this.modelString)
-      .setCharacteristic(Characteristic.FirmwareRevision, HomebridgeDummyVersion)
-      .setCharacteristic(Characteristic.SerialNumber, 'Dummy-' + this.name.replace(/\s/g, '-'));
-  
+    .setCharacteristic(Characteristic.Manufacturer, 'Homebridge')
+    .setCharacteristic(Characteristic.Model, this.modelString)
+    .setCharacteristic(Characteristic.FirmwareRevision, HomebridgeDummyVersion)
+    .setCharacteristic(Characteristic.SerialNumber, 'Dummy-Timer-' + this.name.replace(/\s/g, '-'));
+
   this.cacheDirectory = HomebridgeAPI.user.persistPath();
   this.storage = require('node-persist');
-  this.storage.initSync({dir:this.cacheDirectory, forgiveParseErrors: true});
-  
+  this.storage.initSync({ dir: this.cacheDirectory, forgiveParseErrors: true });
+
   this._service.getCharacteristic(Characteristic.On)
     .on('set', this._setOn.bind(this));
   if (this.dimmer) {
-	this._service.getCharacteristic(Characteristic.Brightness)
-            .on('get', this._getBrightness.bind(this))
-            .on('set', this._setBrightness.bind(this));
-  }
-
-  if (this.reverse) this._service.setCharacteristic(Characteristic.On, true);
-
-  if (this.stateful) {
-	var cachedState = this.storage.getItemSync(this.name);
-	if((cachedState === undefined) || (cachedState === false)) {
-		this._service.setCharacteristic(Characteristic.On, false);
-	} else {
-		this._service.setCharacteristic(Characteristic.On, true);
-	}
+    this._service.getCharacteristic(Characteristic.Brightness)
+      .on('get', this._getBrightness.bind(this))
+      .on('set', this._setBrightness.bind(this));
   }
 
   if (this.dimmer) {
-	var cachedBrightness = this.storage.getItemSync(this.brightnessStorageKey);
-	if ((cachedBrightness == undefined) || cachedBrightness == 0) {
-		this._service.setCharacteristic(Characteristic.On, false);
-		this._service.setCharacteristic(Characteristic.Brightness, 0);
-	} else {
-		this._service.setCharacteristic(Characteristic.On, true);
-		this._service.setCharacteristic(Characteristic.Brightness, cachedBrightness);
-	}
+    var cachedBrightness = this.storage.getItemSync(this.brightnessStorageKey);
+    if ((cachedBrightness == undefined) || cachedBrightness == 0) {
+      this._service.setCharacteristic(Characteristic.On, false);
+      this._service.setCharacteristic(Characteristic.Brightness, 0);
+    } else {
+      this._service.setCharacteristic(Characteristic.On, true);
+      this._service.setCharacteristic(Characteristic.Brightness, cachedBrightness);
+    }
   }
 }
 
-DummySwitch.prototype.getServices = function() {
+DummyTimer.prototype.getServices = function () {
   return [this.informationService, this._service];
 }
 
-function randomize(time) {
-  return Math.floor(Math.random() * (time + 1));
-}
+DummyTimer.prototype._getBrightness = function (callback) {
 
-DummySwitch.prototype._getBrightness = function(callback) {
-
-  if ( ! this.disableLogging ) {
-	this.log("Getting " + "brightness: " + this.brightness);
+  if (!this.disableLogging) {
+    this.log("Getting " + "brightness: " + this.brightness);
   }
 
   callback(null, this.brightness);
 }
 
-DummySwitch.prototype._setBrightness = function(brightness, callback) {
+DummyTimer.prototype._setBrightness = function (brightness, callback) {
 
-  if ( ! this.disableLogging ) {
-	var msg = "Setting brightness: " + brightness
-	this.log(msg);
+  if (!this.disableLogging) {
+    var msg = "Setting brightness: " + brightness
+    this.log(msg);
   }
 
   this.brightness = brightness;
@@ -106,38 +85,31 @@ DummySwitch.prototype._setBrightness = function(brightness, callback) {
   callback();
 }
 
-DummySwitch.prototype._setOn = function(on, callback) {
+DummyTimer.prototype._setOn = function (on, callback) {
 
-  var delay = this.random ? randomize(this.time) : this.time;
   var msg = "Setting switch to " + on
-  if (this.random && !this.stateful) {
-      if (on && !this.reverse || !on && this.reverse) {
-        msg = msg + " (random delay " + delay + "ms)"
-      }
-  }
-  if( ! this.disableLogging ) {
-      this.log(msg);
+  if (!this.disableLogging) {
+    this.log(msg);
   }
 
-  if (on && !this.reverse && !this.stateful) {
-    if (this.resettable) {
-      clearTimeout(this.timer);
+  if (this.isTimer) {
+    if (on) {
+      this._service.setCharacteristic(Characteristic.Brightness, this.defBrightness);
+      this.brightness = this.defBrightness
+
+      this.timer = setInterval(function () {
+        if (this.brightness > 1) {
+          this.brightness = this.brightness - 1
+          this._service.setCharacteristic(Characteristic.Brightness, this.brightness);
+        } else {
+          clearInterval(this.timer);
+          this._service.setCharacteristic(Characteristic.On, false);
+        }
+      }.bind(this), 60000)
+    } else {
+      clearInterval(this.timer);
     }
-    this.timer = setTimeout(function() {
-      this._service.setCharacteristic(Characteristic.On, false);
-    }.bind(this), delay);
-  } else if (!on && this.reverse && !this.stateful) {
-    if (this.resettable) {
-      clearTimeout(this.timer);
-    }
-    this.timer = setTimeout(function() {
-      this._service.setCharacteristic(Characteristic.On, true);
-    }.bind(this), delay);
   }
-  
-  if (this.stateful) {
-	this.storage.setItemSync(this.name, on);
-  }
-  
+
   callback();
 }
